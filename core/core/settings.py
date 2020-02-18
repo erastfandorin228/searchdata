e3eb1d00589f125ae23a7270b635073711397fc0
erastfandorin228@mail.ru <erastfandorin228@mail.ru>
2020-02-18 22:27:03 +0300

    tmpl

--- core/core/settings.py
+++ core/core/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'core' / 'templates'],
+        'DIRS': [PROJECT_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
