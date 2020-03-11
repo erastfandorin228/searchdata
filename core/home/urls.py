2f034069612d89f016e0e598cef55692d4b9de47
erastfandorin228@mail.ru <erastfandorin228@mail.ru>
2020-03-11 20:54:54 +0300

    added index_events

--- core/home/urls.py
+++ core/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("index_events", views.index_events, name='events')
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,9 +1,15 @@
 from platform import system
 
-from django.shortcuts import  render
+from django.shortcuts import render
+
 
 def get_alco():
-    return["Jim Beam", "Vodka", "Pivo"]
+    return ["Jim Beam", "Vodka", "Pivo"]
+
 
 def index(request):
     return render(request, "index.html", {"alco": get_alco(), "title": "Main"})
+
+
+def index_events(request):
+    return render(request, "index_events.html")
