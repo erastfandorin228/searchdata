94f18359bf243da6c7b67545d44345f9eb5c8246
erastfandorin228@mail.ru <erastfandorin228@mail.ru>
2020-03-18 00:46:22 +0300

    add blog

--- core/core/settings.py
+++ core/core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # our apps
     'home',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -16,4 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path('', include('home.urls')), path('admin/', admin.site.urls)]
+urlpatterns = [path('', include('home.urls')), path('admin/', admin.site.urls),
+               path('blog/', include('blog.urls', namespace='blog'))]
