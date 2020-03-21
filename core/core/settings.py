462a118e2a0dbce98afc41f887a23e8a2f398103
erastfandorin228@mail.ru <erastfandorin228@mail.ru>
2020-03-21 19:38:45 +0300

    corr main.css

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # our apps
+    'django_bootstrap_breadcrumbs',
     'home',
     'blog.apps.BlogConfig',
 ]
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
