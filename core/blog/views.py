59369cb9eac3389cd640bac4a93664b1e2678b46
erastfandorin228@mail.ru <erastfandorin228@mail.ru>
2020-04-06 12:40:00 +0300

    similar posts

--- core/blog/views.py
+++ core/blog/views.py
@@ -5,6 +5,7 @@ from .models import Post, Comment
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
@@ -13,7 +14,7 @@ def post_list(request, tag_slug=None):
     # return render(request, 'blog/post/list.html', {'posts': posts})
     object_list = Post.published.all()
     tag = None
-    
+
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
@@ -55,12 +56,21 @@ def post_detail(request, year, month, day, post):
             new_comment.post = post
             # Сохраняем комментарий в базе данных.
             new_comment.save()
-        else:
-            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    # Формирование списка похожих статей
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'new_comment': new_comment,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
